02489d320b30db8f78e0006dc976535bca32477f
superman-saeed <salamwahab@gmail.com>
2019-04-11 04:34:54 -0400

    refactor js

--- asserts/javascript/main.js
+++ asserts/javascript/main.js
@@ -1,15 +1,30 @@
-var element = document.querySelectorAll("a");
-var num = 0;
-element.forEach(function(li,key){
-  li.addEventListener("click",function(e){
-    e.preventDefault();
-    if (num!==key) {
-      element[num].classList.remove("cliked");
-      num =key;
-    }
+var nav = document.querySelectorAll("a");
+var close = document.querySelector("h3");
 
-    li.classList.add("cliked");
-    console.log('cliked');
 
+navSelection(nav);
+closeNav(close);
+
+function navSelection(element){
+  var num = 0;
+  element.forEach(function(li,key){
+    li.addEventListener("click",function(e){
+      e.preventDefault();
+      if (num!==key) {
+        element[num].classList.remove("cliked");
+        num =key;
+      }
+
+      li.classList.add("cliked");
+
+
+    });
   });
-});
+
+
+}
+function closeNav(element){
+  element.addEventListener("click", function(){
+    console.log("close");
+  })
+}
